0ed58b91358fdbb13c49541bd5b815d5354da745
HxshPatil <156885581+hxsh01@users.noreply.github.com>
2024-01-31 18:14:08 +0530

    some changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,10 @@ import React, { useState } from 'react';
 import CoordinateSystem from './components/coordinate_system/coordinate_system';
 
 function App() {
-  const [numberOfPoints, setNumberOfPoints] = useState(0);
 
   return (
     <div className="App">
-      <CoordinateSystem numberOfPoints={numberOfPoints} />
+      <CoordinateSystem />
     </div>
   );
 }
